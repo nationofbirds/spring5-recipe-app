1d11d1667db354afbdbe4bc29193b897b13112e2
sfinch <birdnation@comcast.net>
2020-12-29 16:19:56 -0600

    Completed assignment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT into category (description) VALUES ('American');
+INSERT into category (description) VALUES ('Italian');
+INSERT into category (description) VALUES ('Mexican');
+INSERT into category (description) VALUES ('Fast Food');
+INSERT into unit_of_measure (description) VALUES ('Teaspoon');
+INSERT into unit_of_measure (description) VALUES ('Tablespoon');
+INSERT into unit_of_measure (description) VALUES ('Cup');
+INSERT into unit_of_measure (description) VALUES ('Pinch');
+INSERT into unit_of_measure (description) VALUES ('Ounce');
+
