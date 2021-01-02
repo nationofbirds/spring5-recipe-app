c557937c90ce336a63491b12f96a328ec106b861
sfinch <birdnation@comcast.net>
2021-01-02 15:12:21 -0600

    Completed assignment

---
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -0,0 +1,152 @@
+package guru.springframework.bootstrap;
+
+import guru.springframework.domain.*;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final CategoryRepository categoryRepository;
+    private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public RecipeBootstrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        recipeRepository.saveAll(getRecipes());
+    }
+
+    private List<Recipe> getRecipes() {
+
+        List<Recipe> recipes = new ArrayList<>(2);
+
+        //get UOMs
+        Optional<UnitOfMeasure> eachUomOptional = unitOfMeasureRepository.findByDescription("Each");
+
+        if(!eachUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> tablespoonUomOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        if(!tablespoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> teaspoonUomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        if(!teaspoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> dashUomOptional = unitOfMeasureRepository.findByDescription("Dash");
+
+        if(!dashUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> pintUomOptional = unitOfMeasureRepository.findByDescription("Pint");
+
+        if(!pintUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        Optional<UnitOfMeasure> cupUomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        if(!cupUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM not found");
+        }
+
+        //get UOM Optionals
+        UnitOfMeasure eachUom = eachUomOptional.get();
+        UnitOfMeasure tablespoonUom = tablespoonUomOptional.get();
+        UnitOfMeasure teaspoonUom = teaspoonUomOptional.get();
+        UnitOfMeasure dashUom = dashUomOptional.get();
+        UnitOfMeasure pintUom = pintUomOptional.get();
+        UnitOfMeasure cupUom = cupUomOptional.get();
+
+        //get Categories
+        Optional<Category> americanCategoryOptional = categoryRepository.findByDescription("American");
+
+        if(!americanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category not found");
+        }
+
+        Optional<Category> italianCategoryOptional = categoryRepository.findByDescription("Italian");
+
+        if(!italianCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category not found");
+        }
+
+        Optional<Category> mexicanCategoryOptional = categoryRepository.findByDescription("Mexican");
+
+        if(!mexicanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category not found");
+        }
+
+        Optional<Category> fastFoodCategoryOptional = categoryRepository.findByDescription("Fast Food");
+
+        if(!fastFoodCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category not found");
+        }
+
+        //get Category Optionals
+        Category americanCategory = americanCategoryOptional.get();
+        Category mexicanCategory = mexicanCategoryOptional.get();
+        Category fastFoodCategory = fastFoodCategoryOptional.get();
+
+        // Guac Recipe
+        Recipe guacRecipe = new Recipe();
+        guacRecipe.setDescription("Perfect Guacamole");
+        guacRecipe.setPrepTime(10);
+        guacRecipe.setCookTime(0);
+        guacRecipe.setDifficulty(Difficulty.EASY);
+        guacRecipe.setDirections("1 Cut the avocado, remove flesh: Cut the avocados in half. Remove the pit. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon. (See How to Cut and Peel an Avocado.) Place in a bowl." +
+                "\n" +
+                "2 Mash with a fork: Using a fork, roughly mash the avocado. (Don't overdo it! The guacamole should be a little chunky.)" +
+                "\n" +
+                "3 Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. The acid in the lime juice will provide some balance to the richness of the avocado and will help delay the avocados from turning brown. " +
+                        "Add the chopped onion, cilantro, black pepper, and chiles. Chili peppers vary individually in their hotness. So, start with a half of one chili pepper and add to the guacamole to your desired degree of hotness. " +
+                        "Remember that much of this is done to taste because of the variability in the fresh ingredients. Start with this recipe and adjust to your taste. " +
+                        "Chilling tomatoes hurts their flavor, so if you want to add chopped tomato to your guacamole, add it just before serving." +
+                "\n" +
+                "4 Serve: Serve immediately, or if making a few hours ahead, place plastic wrap on the surface of the guacamole and press down to cover it and to prevent air reaching it. (The oxygen in the air causes oxidation which will turn the guacamole brown.) Refrigerate until ready to serve."
+        );
+
+        Notes guacNotes = new Notes();
+        guacNotes.setRecipeNotes("Quick guacamole: For a very quick guacamole just take a 1/4 cup of salsa and mix it in with your mashed avocados.");
+        guacNotes.setRecipe(guacRecipe);
+        guacRecipe.setNotes(guacNotes);
+
+        guacRecipe.getIngredients().add(new Ingredient("Ripe Avocados", new BigDecimal(2), eachUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Salt", new BigDecimal(.25), teaspoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Fresh Lime or Lemon Juice", new BigDecimal(1), tablespoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Onion", new BigDecimal(2), tablespoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Serrano Chiles, Stems and Seeds Removed", new BigDecimal(2), eachUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Cilantro, Finely Chopped", new BigDecimal(2), tablespoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Ripe Tomato, Seeds and Pulp Removed, Chopped", new BigDecimal(.5), eachUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Red Radish or Jicama, to Garnish", new BigDecimal(1), eachUom, guacRecipe));
+
+        guacRecipe.getCategories().add(americanCategory);
+        guacRecipe.getCategories().add(mexicanCategory);
+
+        recipes.add(guacRecipe);
+
+        return recipes;
+    }
+}
